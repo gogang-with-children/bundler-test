e694367b445658f256bc87212eb7d542d7eace02
GHooN99 <whiteblue0316@gmail.com>
2021-08-11 16:41:38 +0900

    Add example

--- src/getTime.js
+++ src/getTime.js
@@ -1,4 +1,4 @@
 import moment from "moment";
 
-const getCurrentTime = () => moment().format("YYYY-MM-DD-hh-mm-ss");
+const getCurrentTime = () => moment().format("YYYY년 MM시 DD일 hh시 mm분 ss초");
 export default getCurrentTime;
--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
 import getCurrentTime from "./getTime";
 
 const element = document.querySelector("p");
+const price = document.querySelector(".price");
+const btn = document.querySelector("button");
 console.log("hello");
 
 setInterval(() => {
   element.innerHTML = getCurrentTime();
 }, 1000);
 
-const init = async () => {
+const printData = async () => {
   const response = await fetch(
     "https://api.upbit.com/v1/ticker?markets=KRW-BTC"
   );
   const data = await response.json();
   console.log(data);
+  price.innerHTML = `${data[0].trade_price} 원`;
 };
 
-init();
+btn.addEventListener("click", printData);
